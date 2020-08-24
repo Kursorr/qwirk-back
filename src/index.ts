cece2d2fc532e855250427825efaa814957919a8
Ravaniss <senechal.max06@gmail.com>
2020-08-24 17:55:52 +0200

    r

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'module-alias/register'
 
 import express from 'express'
-import bodyParser from "body-parser"
+import bodyParser from 'body-parser'
 import cors from 'cors'
 
 import env from '@core/Environment'
