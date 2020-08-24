863ae7a3e9764c9dd8e2855c6b082c7cb3641d80
Ravaniss <senechal.max06@gmail.com>
2020-08-24 23:15:52 +0200

    Infra - Responses - ApiLogin send error or response

---
+++ src/Auth/Infrastructure/Responses/ApiLogin.ts
@@ -0,0 +1,22 @@
+import ILogin from '@auth_application/Responses/ILogin'
+import User from '@auth_domain/Models/User'
+import { Response } from 'express'
+
+export default class ApiLogin implements ILogin {
+  private readonly response: Response
+
+  public constructor(responseExpress: Response) {
+    this.response = responseExpress
+  }
+
+  send(obj: object | null, err: Error | null): void {
+    if (err) {
+      return this.response.json({
+        message: err.message
+      }).status(400).end()
+    }
+
+    this.response.json(obj).end()
+  }
+
+}
