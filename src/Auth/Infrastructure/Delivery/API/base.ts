aae0a4de0f27d96037326c58a542398c91ceb4ae
Ravaniss <senechal.max06@gmail.com>
2020-08-24 23:09:55 +0200

    Delivery - API - base - Add login route

--- src/Auth/Infrastructure/Delivery/API/base.ts
+++ src/Auth/Infrastructure/Delivery/API/base.ts
@@ -1,11 +1,17 @@
 import express from 'express'
 import RegisterAction from './RegisterAction'
+import LoginAction from "@auth_infrastructure/Delivery/API/LoginAction"
 
 const authRouter = express.Router()
 
 authRouter.post('/register', async (req, res) => {
   const registerAction: RegisterAction = new RegisterAction()
   await registerAction.handle(req, res)
+})
+
+authRouter.post('/login', async (req, res) => {
+  const loginAction: LoginAction = new LoginAction()
+  await loginAction.handle(req, res)
 })
 
 export default authRouter
