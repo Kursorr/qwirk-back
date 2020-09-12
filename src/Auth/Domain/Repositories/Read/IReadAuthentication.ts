f84961a2a25e876b1195da1461b1500650ebf910
Ravaniss <senechal.max06@gmail.com>
2020-09-13 00:53:22 +0200

    Delete PgsqlAuthentication and cut it in twice : Read & Write - CQRS Arch

--- src/Auth/Domain/Repositories/Read/IReadAuthentication.ts
+++ src/Auth/Domain/Repositories/Read/IReadAuthentication.ts
@@ -1,7 +1,6 @@
 import User from '@auth_domain/Models/User'
 import Email from '@auth_domain/ValueObjects/Email'
 
-export interface IAuthentication {
+export interface IReadAuthentication {
   findByEmail(email: Email): Promise<User | null>
-  insert(user: User): Promise<User | null>
 }
