3a5e7ac2de1a62d38673a1a71c7e03e9c4c30db8
Ravaniss <senechal.max06@gmail.com>
2020-09-13 00:53:49 +0200

    Auth Job updated with CQRS

--- src/Auth/Application/Jobs/LoginJob.ts
+++ src/Auth/Application/Jobs/LoginJob.ts
@@ -1,5 +1,5 @@
 import LoginRequest from '@auth_application/DTO/LoginRequest'
-import { IAuthentication } from '@auth_domain/Repositories/IAuthentication'
+import { IReadAuthentication } from '@auth_domain/Repositories/Read/IReadAuthentication'
 import IPassword from '@auth_domain/Services/IPassword'
 import ILogin from '@auth_application/Responses/ILogin'
 import Email from '@auth_domain/ValueObjects/Email'
@@ -8,18 +8,18 @@ import env from '@core/Environment'
 
 export default class LoginJob {
   private readonly loginRequest: LoginRequest
-  private readonly authenticationRepository: IAuthentication
+  private readonly authenticationReadRepository: IReadAuthentication
   private readonly passwordService: IPassword
   private readonly jsonWebTokenService: IJwt
 
   public constructor(
     loginRequest: LoginRequest,
-    authenticationRepository: IAuthentication,
+    authenticationReadRepository: IReadAuthentication,
     passwordService: IPassword,
     jsonWebTokenService: IJwt
   ) {
     this.loginRequest = loginRequest
-    this.authenticationRepository = authenticationRepository
+    this.authenticationReadRepository = authenticationReadRepository
     this.passwordService = passwordService
     this.jsonWebTokenService = jsonWebTokenService
   }
@@ -29,7 +29,7 @@ export default class LoginJob {
 
     let user = null
     try {
-      user = await this.authenticationRepository.findByEmail(email)
+      user = await this.authenticationReadRepository.findByEmail(email)
       await this.passwordService.verify(this.loginRequest.password ?? '', user?.password ?? '')
     } catch (e) {
       return loginResponse.send(null, new Error('Problem with your email or password'))
